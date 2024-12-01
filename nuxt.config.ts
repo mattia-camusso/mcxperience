70c3a883cd2607cb5346a690130665f52235299d
mattia-camusso <mattia.camusso@gmail.com>
2024-12-01 01:23:14 +0100

    third try

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config";
+import { resolve } from "path";
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
+  app: {
+    head: {
+      title: "MC Xperience",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [
+        //{ rel: "icon", type: "image/svg", href: "_nuxt/assets/favicon.ico" },
+      ],
+    },
+  },
+  alias: {
+    "@": resolve(__dirname, "/"),
+  },
+  modules: ["@pinia/nuxt", "@hypernym/nuxt-anime", "@vueuse/nuxt"],
+  /*   ui: {
+    global: true,
+  }, */
   css: ["~/assets/main.scss"],
   postcss: {
     plugins: {
